3d4223abc4b1ddff5fdad9ae47fc094ca81d3a1b
Ziyad Farhan <56755783+ZiyadF296@users.noreply.github.com>
2022-08-14 00:52:06 +0300

    V1 S. Submission.

--- Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     /**
-     *  عرفوا مصفوفة بإسم students
-     *  define an arraylist named students
+     * عرفوا مصفوفة بإسم students
+     * define an arraylist named students
      */
+    final ArrayList<Student> students = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.studentListView);
 
-        /**
-         *  إكتبوا الكود بعد هذا السطر
-         *  ضيفوا عناصر من الكلاس
-         *  add objects from class Student
+        /*
+          إكتبوا الكود بعد هذا السطر
+          ضيفوا عناصر من الكلاس
+          add objects from class Student
          */
 
+        // Boys
+        students.add(new Student("Ahmed", 21, 3, R.drawable.boy));
+        students.add(new Student("Mohamed", 22, 4, R.drawable.boy1));
+        students.add(new Student("Ali", 23, 5, R.drawable.boy2));
 
-        StudentAdapter studentAdapter = new StudentAdapter(this,0,students);
+        // Girls
+        students.add(new Student("Sarah", 20, 4, R.drawable.girl));
+        students.add(new Student("Fatimah", 22, 3, R.drawable.girl1));
 
+        StudentAdapter studentAdapter = new StudentAdapter(this, 0, students);
 
         listView.setAdapter(studentAdapter);
     }
